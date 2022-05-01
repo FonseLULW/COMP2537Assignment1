5900143f3e8fc75da6eb1a86404138174d4da318
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-04-30 19:50:23 -0700

    added front-end functionality on dropdown menu

--- search.js
+++ search.js
@@ -1,4 +1,21 @@
+function changeSearchBy(newSearchBy) {
+    document.querySelector("#searchbyCurrent").innerHTML = newSearchBy;
+}
+
+function setupDropItem(item) {
+    let elem = document.querySelector("#drop" + item);
+    console.log(elem.innerHTML);
+    elem.addEventListener("mousedown", () => {
+        console.log("e");
+        console.log(elem.innerHTML);
+        changeSearchBy(elem.innerHTML);
+    })
+}
+
 function ready() {
+    for (i = 1; i <= 4; i++) {
+        setupDropItem(i);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
