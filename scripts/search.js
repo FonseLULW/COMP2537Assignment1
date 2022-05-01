c547d18cc46524808832601341b049023b865c9a
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-04-30 22:37:24 -0700

    made reformat function, and POKEGENERATIONS

--- scripts/search.js
+++ scripts/search.js
@@ -1,29 +1,64 @@
+let searchBy = document.querySelector("#searchbyCurrent");
+let searchQuery = document.querySelector("#query");
+
 function changeSearchBy(newSearchBy) {
-    document.querySelector("#searchbyCurrent").innerHTML = newSearchBy;
+    searchBy.innerHTML = newSearchBy;
 }
 
 function setupDropItem(item) {
     let elem = document.querySelector("#drop" + item);
-    console.log(elem.innerHTML);
     elem.addEventListener("mousedown", () => {
-        console.log("e");
-        console.log(elem.innerHTML);
         changeSearchBy(elem.innerHTML);
     })
 }
 
+// Reformat query to proper PokeAPI format
+function reformat(oldstring) {
+    let newstring = oldstring;
+    return newstring.trim().toLowerCase().replace(/ /g, "-");
+}
+
+function translate(oldstring) {
+    let newstring = POKEGENERATIONS.get(oldstring);
+    if (newstring == undefined) {
+        return oldstring;
+    } else {
+        return newstring;
+    }
+    
+}
+
 function search() {
-    createPokemonImage(23);
+    switch(searchBy.innerHTML.toLowerCase()) {
+        case "type":
+            break;
+        case "ability":
+            break;
+        case "generation":
+            console.log(translate(reformat(searchQuery.value)));
+            break;
+        default:
+            console.log(reformat(searchQuery.value));
+            // getPokemonByName();
+    }
 }
 
 function ready() {
+    console.log("Translation services available...")
+
+    // dropdown menu setup
     for (i = 1; i <= 4; i++) {
         setupDropItem(i);
     }
+    console.log("Dropdown menu complete...")
 
+    // search button click 
     document.querySelector("#searchPokemon").addEventListener("mousedown", () => {
         search();
     })
+
+
+    console.log("Initialization completed!");
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
--- scripts/universal.js
+++ scripts/universal.js
@@ -1,6 +1,17 @@
 // Total number of existing Pokemon
 const POKECOUNT = 898;
 
+// translate function setup
+const POKEGENERATIONS = new Map();
+POKEGENERATIONS.set('one', 1);
+POKEGENERATIONS.set('two', 2);
+POKEGENERATIONS.set('three', 3);
+POKEGENERATIONS.set('four', 4);
+POKEGENERATIONS.set('five', 5);
+POKEGENERATIONS.set('six', 6);
+POKEGENERATIONS.set('seven', 7);
+POKEGENERATIONS.set('eight', 8);
+
 // container for pokemon images
 const pokeContainer = document.getElementById("pokemon-grid");
 
