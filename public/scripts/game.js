1811f66ca88a1b51e2040bc9303d4fe5bb821658
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-05-28 13:08:12 -0700

    Implemented form validation for game

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,5 +1,17 @@
-function setup() {
+function updateMax(inputFieldElem, newMaxValue) {
+    inputFieldElem.max = newMaxValue;
+}
 
+function setup() {
+    const setup = document.querySelector("#setup");
+    // setup.onsubmit = (e) => {
+    //     e.preventDefault();
+    // };
+
+    const pokesInput = document.querySelector("#pokes");
+    const gridSizeInput = document.querySelector("#dims");
+    // updateMax(pokesInput, gridSizeInput.value)
+    gridSizeInput.onchange = () => updateMax(pokesInput, parseInt(gridSizeInput.value.trim()) / 2);
 }
 
 document.addEventListener("DOMContentLoaded", setup);
